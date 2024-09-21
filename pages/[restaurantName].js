7c48cd92b1b7befc2cf61a0570b6422cb59e9781
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-09-20 23:49:20 -0300

    + cart modal handler

--- pages/[restaurantName].js
+++ pages/[restaurantName].js
@@ -2,7 +2,9 @@ import { FoodList } from "@/components/FoodList"
 import { useParams } from "next/navigation"
 import { useEffect, useState } from "react"
 import { FoodDetailsModal } from "@/components/FoodDetailsModal.jsx"
+import CartIcon from '../public/icon-cart.svg'
 import api from "@/api.js"
+import { CartDetailsModal } from "@/components/CartDetailsModal"
 
 export default function RestaurantHome() {
     const params = useParams()
@@ -11,9 +13,11 @@ export default function RestaurantHome() {
     const [foodError, setFoodError] = useState(false)
     const [showModal, setShowModal] = useState(false)
     const [modalData, setModalData] = useState(null)
+    const [cartModal, setCartModal] = useState(null)
+    const [showCartModal, setShowCartModal] = useState(false)
 
     useEffect(() => {
-        localStorage.removeItem('order')
+        //localStorage.removeItem('order')
         if (params) {
             api.get(`/foods/${params.restaurantName}`)
         .then(res => {
@@ -27,16 +31,16 @@ export default function RestaurantHome() {
         })
         }
 
-        const handleBeforeUnload = (e) => {
-            e.preventDefault()
-            localStorage.removeItem('order')
-        }
+        // const handleBeforeUnload = (e) => {
+        //     e.preventDefault()
+        //     localStorage.removeItem('order')
+        // }
 
-        window.addEventListener('beforeunload', handleBeforeUnload);
+        // window.addEventListener('beforeunload', handleBeforeUnload);
 
-        return () => {
-            window.removeEventListener('beforeunload', handleBeforeUnload);
-        };
+        // return () => {
+        //     window.removeEventListener('beforeunload', handleBeforeUnload);
+        // };
         
     }, [foodLoading, params])
 
@@ -46,15 +50,29 @@ export default function RestaurantHome() {
         // console.log('Abriendo modal...', data)
     }
 
+    const handleShowCartModal = (data) => {
+        setCartModal(data)
+        setShowCartModal(true)
+    }
+
     if (!params) {
         return <div>Cargando...</div>
     }
 
     return (
         <>
-            <h1>Pagina del Restaurante: {params.restaurantName}</h1>
-            <FoodList foodList={foodList} foodLoading={foodLoading} foodError={foodError} handleShowModal={handleShowModal}></FoodList>
-            {showModal && (<FoodDetailsModal data={modalData} setShowModal={setShowModal}></FoodDetailsModal>)}
+            <div className="container mx-auto">
+                <h1>Pagina del Restaurante: {params.restaurantName}</h1>
+                <div className="container flex mx-auto p-4 align-end justify-end">
+                    <button className="flex justify-center align-center items-center gap-2 text-xl bg-white text-black px-2 rounded-lg" onClick={(e) => handleShowCartModal(e)}>
+                        <CartIcon width={50} height={50} fill="#fffffff"></CartIcon>
+                        Ver carrito
+                    </button>
+                </div>
+                <FoodList foodList={foodList} foodLoading={foodLoading} foodError={foodError} handleShowModal={handleShowModal}></FoodList>
+                {showModal && (<FoodDetailsModal data={modalData} setShowModal={setShowModal}></FoodDetailsModal>)}
+                {showCartModal && (<CartDetailsModal cartModal={JSON.parse(localStorage.getItem('order'))} setShowCartModal={setShowCartModal}></CartDetailsModal>)}
+            </div>
         </>
     )
 }
