acfc333a5938cbed63ae389d461611a429e881d5
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-09-20 23:50:42 -0300

    modal with cart details

--- components/CartDetailsModal.jsx
+++ components/CartDetailsModal.jsx
@@ -1,21 +1,30 @@
 import { useEffect, useState } from "react"
+import { FoodCartCard } from "./FoodCartCard"
 
-export function CartModalDetails(props) {
-    const [cartData, setCartData] = useState([])
-
+export function CartDetailsModal(props) {
+    // const [cartData, setCartData] = useState(null)
+    // const [loadingCartData, setLoadingCartData] = useState(true)
+    
     useEffect(() => {
-        setCartData(JSON.parse(localStorage.getItem('order')))
+        
     }, [])
-
-    if (!props.isOpen) return null
 
     return (
         <>
-            <div className="cart-modal-container">
-                <div className="cart-modal-main">
-                    <div onClick={(e) => props.setShowCartModal(false)}>X</div>
-                    {console.log("modal")}
+            <div className="cart-modal-container absolute flex justify-center items-center top-0 left-0 w-full h-full bg-[#0a0a0a36] z-0">
+                <div className="cart-modal-main relative flex flex-col gap-8 z-10 bg-white p-8 rounded-lg w-1/2 items-center text-black">
+                <div onClick={(e) => props.setShowCartModal(false)}>X</div>
+                {props.cartModal && (
+                    <>
+                        {Object.entries(props.cartModal.items).map(([key, value]) => (
+                            <FoodCartCard key={key} data={value} />
+                        ))}
+                    </>
+                )}
+                {!props.cartModal && <h2>No tienes productos en tu carrito.</h2>}
                 </div>
+                
+                    
             </div>
         </>
     )
