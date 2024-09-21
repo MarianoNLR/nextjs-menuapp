066c50e43230538d661965063a21ba364759352e
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-09-20 22:16:25 -0300

    config files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+import plugin from 'tailwindcss'
+
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
